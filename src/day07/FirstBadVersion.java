0ac011b484f3c01d6af7c41c498515fc22be683b
wuhaojie <w19961009@126.com>
2016-08-10 13:12:31 +0800

    update FirstBadVersion.

--- src/day07/FirstBadVersion.java
+++ src/day07/FirstBadVersion.java
@@ -29,6 +29,20 @@ public class FirstBadVersion {
     }
 
 
+    public static int firstBadVersion_good(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
     public static void main(String[] args) {
         System.out.println(firstBadVersion(3));
     }
